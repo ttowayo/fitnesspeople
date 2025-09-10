8f523c0a70f5ed66be22a39eb77e282223a87aa6
ttowayo <ttowayoyo@gmail.com>
2025-09-10 16:43:09 +0900

    first

---
+++ base/page/brand/brand.js
@@ -0,0 +1,26 @@
+jQuery1_11_2(function () {
+  /*이곳은 건들지 마세요 Start*/
+  const locations = location.href;
+  const locationSplit = locations.split("?");
+  const encodedString_brand = locationSplit[1];
+  const decodedString_brand = decodeURIComponent(encodedString_brand);
+  //alert(decodedString)
+  /*메뉴 컬러 분기*/
+
+  const elementsWithDataName = document.querySelectorAll(
+    ".board-list[data-cate]"
+  );
+  $(".board-list[data-cate]").each(function () {
+    if ($(this).data("cate") === decodedString_brand) {
+      // CSS를 변경하고 싶은 경우 여기에서 요소에 스타일을 추가하거나 클래스를 변경할 수 있습니다.
+      $(this).find("a").css("color", "#f0468c"); // 예시로 글자색을 빨간색으로 변경합니다.
+    }
+  });
+  /*board 타이틀은 이름이 달라 연동이 안되는 관계로 강제로 적용시킵니다*/
+  if (decodedString_brand === "brandstory") {
+    $(".pageTitle").find(">h2").text("brand story").css("color", "#f0468c");
+  }
+  if (decodedString_brand === "doongs") {
+    $(".pageTitle").find(">h2").text("doongs").css("color", "#f0468c");
+  }
+});
---
+++ base/page/program/program.js
@@ -0,0 +1,24 @@
+jQuery1_11_2(function () {
+  /*이곳은 건들지 마세요 Start*/
+  const locations = location.href;
+  const locationSplit = locations.split("?");
+  const encodedString_program = locationSplit[1];
+  const decodedString_program = decodeURIComponent(encodedString_program);
+  //alert(decodedString)
+  /*메뉴 컬러 분기*/
+
+  $(".board-list[data-cate]").each(function () {
+    if ($(this).data("cate") === decodedString_program) {
+      // CSS를 변경하고 싶은 경우 여기에서 요소에 스타일을 추가하거나 클래스를 변경할 수 있습니다.
+      $(this).find("a").css("color", "#f0468c"); // 예시로 글자색을 빨간색으로 변경합니다.
+    }
+  });
+  /*board 타이틀은 이름이 달라 연동이 안되는 관계로 강제로 적용시킵니다*/
+
+  if (decodedString_program === "pt") {
+    $(".pageTitle").find(">h2").text("PT").css("color", "#f0468c");
+  }
+  if (decodedString_program === "fitt") {
+    $(".pageTitle").find(">h2").text("FITT").css("color", "#f0468c");
+  }
+});
